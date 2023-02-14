f08e88043197fa1b9cfbc85fd780079d95313678
stasvitalich <stasvitalich1994@gmail.com>
2023-02-14 14:40:42 +0600

    Binding was added. Gradle was updated.

--- app/src/main/java/com/example/saveongroceries/MainActivity.kt
+++ app/src/main/java/com/example/saveongroceries/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.saveongroceries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import com.example.saveongroceries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
